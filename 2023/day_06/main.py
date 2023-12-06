6aa3a6c498ed0a0ab5b1d3169f1289f0f8cc839a
jacobhoernlein <jacobhoernlein@gmail.com>
2023-12-05 23:49:59 -0600

    pretty

--- 2023/day_06/main.py
+++ 2023/day_06/main.py
@@ -3,21 +3,24 @@
 from math import sqrt, floor, ceil
 
 
-def get_margin(time, dist) -> tuple[int, int]:
+def get_num_ways(time: int, dist: int) -> int:
     """Uses the quadratic formula to get the min and max time needed to
     beat the given distance, then returns the number of times possible.
     """
     
     min_t = floor(((time - sqrt(time**2 - 4*dist)) / 2) + 1)
     max_t = ceil(((time + sqrt(time**2 - 4*dist)) / 2) - 1)
+    
     return max_t - min_t + 1
 
 def part_one(times: list[int], distances: list[int]) -> int:
     """Finds the margin for each game and returns the product."""
 
     margin = 1
+
     for time, dist in list(zip(times, distances)):
-        margin *= get_margin(time, dist)
+        margin *= get_num_ways(time, dist)
+    
     return margin
 
 def part_two(times: list[int], distances: list[int]) -> int:
@@ -26,17 +29,15 @@ def part_two(times: list[int], distances: list[int]) -> int:
     time = int("".join([str(n) for n in times]))
     dist = int("".join([str(n) for n in distances]))
     
-    return get_margin(time, dist)
+    return get_num_ways(time, dist)
 
 
 if __name__ == "__main__":
     with open("input.txt") as fp:
-        lines = [line[:-1] for line in fp]
-
-    times, distances = [
-        [int(n) for n in l.split(":")[1].split(" ") if n.isnumeric()]
-        for l in lines
-    ]
+        times, distances = [
+            [int(n) for n in line[:-1].split(":")[1].split(" ") if n.isnumeric()]
+            for line in fp
+        ]
 
     print("Part One:", part_one(times, distances))
     print("Part Two:", part_two(times, distances))
