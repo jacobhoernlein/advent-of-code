e4ea8a71425c3764172ddfa7bf77a666bfee525a
jacobhoernlein <jacobhoernlein@gmail.com>
2023-12-06 11:22:02 -0600

    comments/error handling day 6.

--- 2023/day_06/main.py
+++ 2023/day_06/main.py
@@ -5,15 +5,29 @@ from math import sqrt, floor, ceil
 
 def get_num_ways(time: int, dist: int) -> int:
     """Uses the quadratic formula to get the min and max time needed to
-    beat the given distance, then returns the number of times possible.
+    beat the given distance, then returns the range of those times.
+
+    Based on the fact that the equation for distance traveled is
+        d = t(T-t)
+    Where T is the length of the race and t is the time spent winding
+    the boat. 
+    
+    Thus, the times t for which the boat travels the record distance D
+    can be found by using the quadratic formula on the following:
+        t^2 - Tt + D = 0
     """
     
-    sqrt_disc = sqrt(time**2 - 4*dist)
+    try:
+        sqrt_disc = sqrt(time**2 - 4*dist)
+    except ValueError:  # Not enough time for given distance.
+        return 0
+
+    # We use floor/ceil because we need the next largest/smallest
+    # integer for the given solution of t.
+    min_t = floor((time - sqrt_disc)/2 + 1)
+    max_t = ceil((time + sqrt_disc)/2 - 1)
     
-    min_t = floor((time - sqrt_disc) / 2 + 1)
-    max_t = ceil((time + sqrt_disc) / 2 - 1)
-    
-    return max_t - min_t + 1
+    return max_t - min_t + 1 if max_t >= min_t else 0
 
 def part_one(times: list[int], distances: list[int]) -> int:
     """Finds the number of ways each game can be beat and returns the
