07a866f7e142db6afa336721000b5397287906c8
jacobhoernlein <jacobhoernlein@gmail.com>
2023-12-07 17:20:55 -0600

    everything is a hand.

--- 2023/day_07/main.py
+++ 2023/day_07/main.py
@@ -3,66 +3,25 @@
 from functools import reduce
 
 
-def get_type(s: str, wild=False) -> int | float:
-    """Returns the strength of the hand's type as a number, based on if
-    it's wild or not, in a range from 1 (high card) to 5 (five of a kind).
-    """
-    
-    # Counts cards in the hand.
-    card_count = {c: 0 for c in s}
-    for c in s:
-        card_count[c] += 1
-
-    # Returns a list with the largest number of cards first, then
-    # the next largest, and so on. For example, for a full house,
-    # the list will be [3, 2]. For a two pair it will be [2, 2, 1]
-    # and so on. We exclude jokers at first, because we treat
-    # ranking differently based on if the hand is wild.
-    count_list = sorted(
-        [v for k, v in card_count.items() if k != "J" and v > 0], 
-        reverse=True
-    )
-
-    try:
-        if wild:    
-            # Add jokers to highest count.
-            count_list[0] += card_count["J"]
-        else:       
-            # Add jokers back in and sort again.
-            count_list.append(card_count["J"])
-            count_list.sort(reverse=True)
-    except IndexError:  # Wild and all jokers.
-        return 5
-    except KeyError:    # No jokers.
-        pass
-
-    # The hand is a two pair or a full house.
-    if count_list[0] in (2, 3) and count_list[1] == 2:
-        count_list[0] += 0.5
-
-    return count_list[0]
-
-
-STRENGTHS = {
-    "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
-    "9": 9, "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14
-}
-
-STRENGTHS_WILD = {
-    "J": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, 
-    "8": 8, "9": 9, "T": 10, "Q": 12, "K": 13, "A": 14
-}
-
 class Hand:
+    """Class representing a hand of Camel Poker cards. Constructed with
+    a string representing the cards, and a boolean representing whether
+    or not jokers should be treated as wild. Supports comparison
+    between instances."""
+
+    __card_strengths = {
+        "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
+        "9": 9, "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14
+    }
 
     def __init__(self, s: str, wild=False):
-        self.type = get_type(s, wild)
+        self.type = Hand.__get_type(s, wild)
 
-        # Translates each character in the string to an integer value
-        # representing its relative weight using the appropriate
-        # dictionary.
-        strengths = STRENGTHS_WILD if wild else STRENGTHS
-        self.cards = [strengths[c] for c in s]
+        # Updates the strength of a joker if the hand is wild or not,
+        # then translates each character in the string to an integer
+        # value representing its relative weight.
+        Hand.__card_strengths["J"] = 1 if wild else 11
+        self.__card_strengths = [Hand.__card_strengths[c] for c in s]
 
     def __lt__(self, other: "Hand"):
         """Compares hands first based on their type, then on individual
@@ -72,11 +31,51 @@ class Hand:
         if self.type != other.type:
             return self.type < other.type
         
-        for a, b in zip(self.cards, other.cards):
+        for a, b in zip(self.__card_strengths, other.__card_strengths):
             if a != b:
                 return a < b
         
         return False
+    
+    @staticmethod
+    def __get_type(s: str, wild=False) -> int | float:
+        """Returns the strength of the hand's type as a number, based on if
+        it's wild or not, in a range from 1 (high card) to 5 (five of a kind).
+        """
+        
+        # Counts cards in the hand.
+        card_count = {c: 0 for c in s}
+        for c in s:
+            card_count[c] += 1
+
+        # Returns a list with the largest number of cards first, then
+        # the next largest, and so on. For example, for a full house,
+        # the list will be [3, 2]. For a two pair it will be [2, 2, 1]
+        # and so on. We exclude jokers at first, because we treat
+        # ranking differently based on if the hand is wild.
+        count_list = sorted(
+            [v for k, v in card_count.items() if k != "J" and v > 0], 
+            reverse=True
+        )
+
+        try:
+            if wild:    
+                # Add jokers to highest count.
+                count_list[0] += card_count["J"]
+            else:       
+                # Add jokers back in and sort again.
+                count_list.append(card_count["J"])
+                count_list.sort(reverse=True)
+        except IndexError:  # Wild and all jokers.
+            return 5
+        except KeyError:    # No jokers.
+            pass
+
+        # The hand is a two pair or a full house.
+        if count_list[0] in (2, 3) and count_list[1] == 2:
+            count_list[0] += 0.5
+
+        return count_list[0]
 
 
 def get_winnings(split_lines: list[tuple[str, str]], wild=False) -> int:
